52085a9343f8b7ac2d708480dadcebbc89e03741
Arun Chennadi <aruntejbombay@gmail.com>
2017-11-27 14:00:40 -0800

    testing prebuild

--- config.sh
+++ config.sh
@@ -5,10 +5,20 @@ function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
     :
+    echo "pre-build steps"
+    if [ -n "$IS_OSX" ]; then
+        echo "osx stuff"
+        #brew update
+        #brew install libev
+    else
+        # Install a system package required by our library
+        echo "yoyo"
+    fi
 }
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
-    python --version
-    python -c 'import sys; import yourpackage; sys.exit(yourpackage.test())'
+    #python --version
+    #python -c 'import sys; import yourpackage; sys.exit(yourpackage.test())'
+    echo "running tests"
 }
