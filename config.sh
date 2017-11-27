68c4c1b99c30b611a397ba0bf2b7534f59bba8b6
Arun Chennadi <aruntejbombay@gmail.com>
2017-11-27 00:56:05 -0800

    testing

--- config.sh
+++ config.sh
@@ -2,24 +2,33 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
-function before_install {
+
+function pre_build{
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
     #:
     echo "pre-build steps"
+
+}
+
+function before_install {
     if [ -n "$IS_OSX" ]; then
         echo "osx stuff"
         #brew update
         #brew install libev
     else
         # Install a system package required by our library
+
+    fi
+}
+
+function build_wheel {
         echo "manylinux1 stuff"
         wget http://repository.it4i.cz/mirrors/repoforge/redhat/el5/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el5.rf.x86_64.rpm
         rpm -i rpmforge*
         yum install -y libev libev-devel
-    fi
-}
 
+}
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     #python --version
