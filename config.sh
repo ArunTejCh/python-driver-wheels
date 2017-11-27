469b5b9ac71775d2b4e35a61000391fc1d8a4bcf
Arun Chennadi <aruntejbombay@gmail.com>
2017-11-27 01:24:45 -0800

    testing

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ function pre_build{
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
     #:
-    echo "pre-build steps"
+    #echo "pre-build steps"
 
 }
 
@@ -18,15 +18,15 @@ function before_install {
         #brew install libev
     else
         # Install a system package required by our library
-
+        echo "yoyo"
     fi
 }
 
 function build_wheel {
-        echo "manylinux1 stuff"
-        wget http://repository.it4i.cz/mirrors/repoforge/redhat/el5/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el5.rf.x86_64.rpm
-        rpm -i rpmforge*
-        yum install -y libev libev-devel
+        #echo "manylinux1 stuff"
+        #wget http://repository.it4i.cz/mirrors/repoforge/redhat/el5/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el5.rf.x86_64.rpm
+        #rpm -i rpmforge*
+        #yum install -y libev libev-devel
 
 }
 function run_tests {
